03e8ef68b859fb01b7755cc4b00c0e6206cbbdb7
randyma <435420057@qq.com>
2022-05-19 16:57:48 +0800

    add error type

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -47,35 +47,71 @@ const (
 	RUNTIME_CTX_CLIENT_PORT      RuntimeContextValueKT = "client_port"      // 客户端端口
 )
 
-// Logger公开了一个日志框架以在模块中使用。它公开了特定于级别的日志函数和一组通用函数，以实现兼容性
+/*
+Error is used to indicate a failure in code. The message and code are returned to the client.
+If an Error is used as response for a HTTP/gRPC request, then the server tries to use the error value as the gRPC error code. This will in turn translate to HTTP status codes.
+For more information, please have a look at the following:
+	https://github.com/grpc/grpc-go/blob/master/codes/codes.go
+	https://github.com/grpc-ecosystem/grpc-gateway/blob/master/runtime/errors.go
+	https://golang.org/pkg/net/http/
+*/
+type Error struct {
+	Message string
+	Code    int
+}
+
+// Error returns the encapsulated error message.
+func (e *Error) Error() string {
+	return e.Message
+}
+
+/*
+NewError returns a new error. The message and code are sent directly to the client. The code field is also optionally translated to gRPC/HTTP code.
+	runtime.NewError("Server unavailable", 14) // 14 = Unavailable = 503 HTTP status code
+*/
+func NewError(message string, code int) *Error {
+	return &Error{Message: message, Code: code}
+}
+
+/*
+Logger exposes a logging framework to use in modules. It exposes level-specific logging functions and a set of common functions for compatibility.
+*/
 type Logger interface {
-
-	// Debug 调试
+	/*
+		Log a message with optional arguments at DEBUG level. Arguments are handled in the manner of fmt.Printf.
+	*/
 	Debug(format string, v ...interface{})
-
-	// Info 信息
+	/*
+		Log a message with optional arguments at INFO level. Arguments are handled in the manner of fmt.Printf.
+	*/
 	Info(format string, v ...interface{})
-
-	// Warn 警告
+	/*
+		Log a message with optional arguments at WARN level. Arguments are handled in the manner of fmt.Printf.
+	*/
 	Warn(format string, v ...interface{})
-
-	// Error 错误
+	/*
+		Log a message with optional arguments at ERROR level. Arguments are handled in the manner of fmt.Printf.
+	*/
 	Error(format string, v ...interface{})
-
-	// WithField 返回设置了指定字段的记录器，以便它们包含在后续的日志调用中
+	/*
+		Return a logger with the specified field set so that they are included in subsequent logging calls.
+	*/
 	WithField(key string, v interface{}) Logger
-
-	// WithFields 返回设置了指定字段的记录器，以便在后续日志调用中包含这些字段。
+	/*
+		Return a logger with the specified fields set so that they are included in subsequent logging calls.
+	*/
 	WithFields(fields map[string]interface{}) Logger
-
-	// Fields 返回此记录器中设置的字段
+	/*
+		Returns the fields set in this logger.
+	*/
 	Fields() map[string]interface{}
 }
 
-// Initializer 初始化器用于向服务器注册各种回调函数。
-// 当服务器在server start上加载模块时调用函数时，InitModule函数可以将其作为输入参数使用。
-//
-// 注意：您不能缓存对它的引用，并在以后再次使用它，因为这可能会产生意外的副作用
+/*
+Initializer is used to register various callback functions with the server.
+It is made available to the InitModule function as an input parameter when the function is invoked by the server when loading the module on server start.
+NOTE: You must not cache the reference to this and reuse it as a later point as this could have unintended side effects.
+*/
 type Initializer interface {
 	RegisterRpc(id string, fn func(ctx context.Context, logger Logger, db *sql.DB, na LinnaModule, payload string) (string, error)) error
 }
